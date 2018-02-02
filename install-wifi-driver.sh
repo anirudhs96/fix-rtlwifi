dacf68d7990a592fdd16088a5606f9c258db4a7c
Anirudh Suresh <anirudhsuresh96@gmail.com>
2018-02-02 18:21:34 +0530

    fixed download file name

--- install-wifi-driver.sh
+++ install-wifi-driver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~/Downloads
-wget "https://github.com/lwfinger/rtlwifi_new/archive/master.zip"
+wget "https://github.com/lwfinger/rtlwifi_new/archive/master.zip" -O rtlwifi-new.zip
 unzip rtlwifi_new.zip
 cd rtlwifi_new-master
 make
