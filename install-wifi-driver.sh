5c4fd40b7ee7d38890d84744a3ea01e638c71833
Anirudh Suresh <anirudhsuresh96@gmail.com>
2018-02-02 18:14:01 +0530

    Installs new driver and permanently changes wifi parameters

---
+++ install-wifi-driver.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd ~/Downloads
+wget "https://github.com/lwfinger/rtlwifi_new/archive/master.zip"
+unzip rtlwifi_new.zip
+cd rtlwifi_new-master
+make
+sudo make install
+sudo modprobe -rv rtl8723be
+sudo modprobe -v rtl8723be ant_sel=2
+sudo ip link set wlo1 up
+echo "options rtl8723be ant_sel=2" | sudo tee /etc/modprobe.d/50-rtl8723be.conf
